e5475b5ebfb834f58ee5934cc0095604da6645ab
Pilleownpm <pilleowo@gmail.com>
2023-06-30 13:35:28 +0200

    backend init

---
+++ back-end/quizapp/src/test/java/pl/pilleow/quizapp/QuizappApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pilleow.quizapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
